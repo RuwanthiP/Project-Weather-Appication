820d4af9039e43565a7d6943c7fa2c00fb0f88ee
Ruwanthi Prasadini <you@example.com>
2021-09-03 13:03:30 +0530

    feath:search function created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [query, setQuery] = useState('');
+    const search = async (e) => {
+      if(e.key === 'Enter') {
+          const data = await fetchWeather(query);
+
+          console.log(data);
+      }
+
+  }
+
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+    <div className="main-container">
+        <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+        
+    </div>);
 }
 
 export default App;
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL='api.openweathermap.org/data/2.5/weather';
+const URL='https://api.openweathermap.org/data/2.5/weather';
 const API_KEY='ab6a194b259af0efd19bab9c2273063e';
 
 export const fetchWeather = async (query) => {
